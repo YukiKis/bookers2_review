83562567ce7eade03ba134ebf456a991f875eb7b
EC2 Default User <ec2-user@ip-172-31-35-215.ec2.internal>
2020-12-20 10:25:14 +0000

    feature comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,23 @@
 class CommentsController < ApplicationController
   def create 
+    @book = Book.find(params[:book_id])
+    @comment = current_user.comments.new(comment_params)
+    @comment.book = @book
+    if @comment.save
+      redirect_to book_path(@book)
+    else
+      @user = @book.user
+      render "books/show"
+    end
   end
   
   def destroy
+    @book = Book.find(params[:book_id])
+    Comment.find(params[:id]).destroy
   end
+  
+  private
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
-  has_many :books, dependent: :destroy
+  has_many :comments, dependent: :destroy
   
   validates :title, presence:  true
   validates :opinion, presence: true
